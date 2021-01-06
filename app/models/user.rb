f7996aeba4aafa76d3daa947908998c75d88ab54
sappy920 <sae.920@gamil.com>
2021-01-06 17:35:16 +0900

    正規表現の繰り返し記述を省いたvalidationの追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,14 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, {with: PASSWORD_REGEX, message: "は半角英数字で入力してください" }
        
-  with_options presence: true do
-        validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください"}
-        validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください"}
-        validates :kana_family_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-        validates :kana_last_name , format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+  with_options presence: true , format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください"} do
+        validates :family_name
+        validates :last_name
+  end
+
+  with_options presence: true , format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"} do
+        validates :kana_family_name
+        validates :kana_last_name 
   end
 
 end
