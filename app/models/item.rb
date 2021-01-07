c96848f9c73fe9eb992e4f40a33a90aba7788522
sappy920 <sae.920@gamil.com>
2021-01-07 12:40:00 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :prefecture, :delivery_fee, :delivery_date
 end
