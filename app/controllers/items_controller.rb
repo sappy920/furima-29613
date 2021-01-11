18597f30b16f7364da2c04e8f2433d34379d6ca9
sappy920 <sae.920@gamil.com>
2021-01-11 12:27:59 +0900

    商品のバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   def create
     @item= Item.new(item_params)
     if @item.save
-      redirect_to items_path
+      redirect_to root_path
     else
       render :new
     end
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:price, :name, :instruction, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id)
+    params.require(:item).permit(:price, :name, :instruction, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :image).merge(user_id: current_user.id)
   end
 end
