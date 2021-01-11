f10a23d9d9b75f1c6cacbcf783e95f2d7c1233f7
sappy920 <sae.920@gamil.com>
2021-01-11 17:36:42 +0900

    ログインしたユーザーのみ商品出品できるための実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new]
+  
   def index
   end
 
   def new
     @item = Item.new
   end
-
 
   def create
     @item= Item.new(item_params)
