27758a26afc16541cb16eec4c937ea1906f5a56c
sappy920 <sae.920@gamil.com>
2021-01-15 20:39:20 +0900

    コントローラ内の繰り返し記述のまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
+
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.all.includes(:user)
@@ -10,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item= Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -19,18 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id 
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -40,6 +39,10 @@ class ItemsController < ApplicationController
 
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:price, :name, :instruction, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :image).merge(user_id: current_user.id)
