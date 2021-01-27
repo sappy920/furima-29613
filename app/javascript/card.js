c00e93fe34831197261b30b1fb7e28b99b33bea9
sappy920 <sae.920@gamil.com>
2021-01-27 16:17:00 +0900

    不要なコメントアウトの削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -4,7 +4,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    // console.log(form)
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
@@ -15,7 +14,6 @@ window.addEventListener("DOMContentLoaded", () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-    //console.log(card)
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
